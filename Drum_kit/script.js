5d870896e82a28b20ea06b901900e775c2bbb4a7
shashank <shekhar1245shashank@gmail.com>
2021-02-17 23:26:12 +0530

    Drum kit

---
+++ Drum_kit/script.js
@@ -0,0 +1,45 @@
+var noOfButton=document.querySelectorAll(".drum").length;
+
+for(var i=0;i<noOfButton;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){  
+       
+       var buttonInnerHTML=this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var audio=new Audio('sounds/crash.mp3')
+                audio.play() 
+                break;
+            case "a":
+                var crash=new Audio('sounds/kick-bass.mp3')
+                crash.play() 
+                break;
+            case "s":
+                var kick=new Audio('sounds/snare.mp3')
+                kick.play() 
+                break;
+            case "d":
+                var snare=new Audio('sounds/tom-1.mp3')
+                snare.play() 
+                break;
+            case "j":
+                var tom=new Audio('sounds/tom-2.mp3')
+                tom.play() 
+                break;
+            case "k":
+                var  tom2=new Audio('sounds/tom-2.mp3')
+              tom2.play() 
+                break;
+            case "l":
+                var tom2=new Audio('sounds/tom-3.mp3')
+                tom2.play() 
+                break;
+            default:
+                console.log('buttonInnerHTML')
+        }       
+     })
+}
+
+/*
+ var audio=new Audio('sounds/snare.mp3')
+        audio.play() 
+*/
