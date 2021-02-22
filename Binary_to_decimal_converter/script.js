4c35b88285bf8cb93fb88c00df9ea1ba134d87a1
shashank <shekhar1245shashank@gmail.com>
2021-02-23 01:22:00 +0530

    Binary to Decimal Converter
    Signed-off-by: shashank <shekhar1245shashank@gmail.com>

---
+++ Binary_to_decimal_converter/script.js
@@ -0,0 +1,17 @@
+
+let binaryNum = document.getElementById("binaryNum").nodeValue
+
+/*
+var d = document.getElementById('binaryNum')
+let decimalNum = document.getElementById('decimalNum')
+
+
+
+decimalNum.addEventListener('click',function(){
+    return num.split('').reverse().reduce(function(x, y, i){
+        return (y === '1') ? x + Math.pow(2, i) : x;
+      }, 0);
+})
+*/
+
+
